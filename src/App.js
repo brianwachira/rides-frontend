e0a84571a01d8c544d503ab3879cb85e36e0fb05
brianwachira <brianwachira7@gmail.com>
2021-05-23 16:11:15 +0300

    [FEATURE] : import pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import Login from "./Pages/Auth/Login/Login.js";
+// pages
+import Login from "./Pages/Auth/Login/Login";
+import PageNotFound from "./Pages/404/PageNotFound";
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/auth/login" component={Login}/>
+        <Route component={PageNotFound}/>
       </Switch>
     </BrowserRouter>
   );
