e0a6881ae77d1c5f434cc08330e88d98c12324b8
brianwachira <brianwachira7@gmail.com>
2021-05-27 23:55:46 +0300

    [FEATURE] : dashboard

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,37 @@
 import React from "react" 
-import Map from "../../Components/Map/Map"
+import Card from "../../Components/Card/Card"
+import SideBar from '../../Components/SideBar/SideBar'
+import './Home.scss'
 const Home = () => {
 
     return (
         <>
-            <h1>Hello</h1>
-            <Map/>
+            <div className="row">
+                <SideBar/>
+                <div className="col-md-4">
+                    <Card
+                        icon={<i className="fa fa-globe fa-4x icon-custom" aria-hidden="true"></i>}
+                        label='No. Of Rides Today'
+                        number='20'
+                    />
+                </div>
+                <div className="col-md-4">
+                    <Card
+                        icon={<i className="fa fa-motorcycle fa-4x icon-custom" aria-hidden="true"></i>}
+                        label='No. Of Active Rides'
+                        number='20'
+                        />
+
+                </div>
+                <div className="col-md-4">
+                    <Card
+                        icon={<i className="fa fa-check-circle fa-4x icon-custom" aria-hidden="true"></i>}
+                        label='No. Of Rides Today'
+                        number='20'
+                        />
+
+                </div>
+            </div>
         </>
     )
 
