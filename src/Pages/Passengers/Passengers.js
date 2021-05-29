d1caf08cc1b4f706c0232f869dcca1c49c8ad446
brianwachira <brianwachira7@gmail.com>
2021-05-29 19:44:34 +0300

    [FIX] - wrap table in class table-responsive - make table header color to blue

--- src/Pages/Passengers/Passengers.js
+++ src/Pages/Passengers/Passengers.js
@@ -42,7 +42,7 @@ const Passengers = () => {
         <>
          <main className="container-fluid">
              <div className="row">
-                 <div className="col-md-3 ps-0">
+                 <div className="col-md-3 px-0">
                     <SideBar/>
                  </div>
                  <div className="col-md-9">
@@ -54,38 +54,40 @@ const Passengers = () => {
                         phoneNumber={phoneNumber}
                         setPhoneNumber={setPhoneNumber}
                         handleSubmit = {createPassenger}/>
-                    <table className="table table-striped table-hover mt-5">
-                        <thead>
-                            
-                            <tr>
-                                <th>#</th>
-                                <th>name</th>
-                                <th>phone number</th>
-                                <th>Options</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {passengers.map((passenger,index )=>
-                                <tr
-                                    key={passenger.id}>
-                                    <td>{index}</td>
-                                    <td>{passenger.name}</td>
-                                    <td>{passenger.phoneNumber}</td>
-                                    <td>
-                                        <div className="btn-group">
-                                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
-                                                
-                                            </button>
-                                            <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
-                                                <li className="dropdown-item" >Delete Passenger Account</li>
-                                            </ul>
-                                        </div> 
-                                    </td>
+                    
+                    <div className="table-responsive">
+                        <table className="table table-striped table-hover mt-5">
+                            <thead className="table-blue text-light">
+                                
+                                <tr>
+                                    <th>#</th>
+                                    <th>name</th>
+                                    <th>phone number</th>
+                                    <th>Options</th>
                                 </tr>
-                                )}
-                        </tbody>
-                    </table>
-                     
+                            </thead>
+                            <tbody>
+                                {passengers.map((passenger,index )=>
+                                    <tr
+                                        key={passenger.id}>
+                                        <td>{index}</td>
+                                        <td>{passenger.name}</td>
+                                        <td>{passenger.phoneNumber}</td>
+                                        <td>
+                                            <div className="btn-group">
+                                                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
+                                                    
+                                                </button>
+                                                <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
+                                                    <li className="dropdown-item" >Delete Passenger Account</li>
+                                                </ul>
+                                            </div> 
+                                        </td>
+                                    </tr>
+                                    )}
+                            </tbody>
+                        </table>
+                    </div>
                  </div>
              </div>
          </main>
