b378cff0d14d9da6be46caf2061b912d2ae0e06c
brianwachira <brianwachira7@gmail.com>
2021-05-29 19:40:38 +0300

    [FIX] - wrap table in class table-responsive - make table header color to blue - set badge on suspended option based on data gotten

--- src/Pages/Drivers/Drivers.js
+++ src/Pages/Drivers/Drivers.js
@@ -45,7 +45,7 @@ const Drivers = () => {
 
         <main className="container-fluid">
             <div className="row">
-                <div className="col-md-3 ps-0">
+                <div className="col-md-3 px-0">
                     <SideBar/>
                 </div>
                 <div className="col-md-9">
@@ -57,45 +57,47 @@ const Drivers = () => {
                             phoneNumber={phoneNumber}
                             setPhoneNumber={setPhoneNumber}
                             handleSubmit = {createDriver}/>
-                    <table className="table table-striped table-hover mt-5">
-                        <thead>             
-                            <tr>
-                                <th>#</th>
-                                <th>name</th>
-                                <th>phone number</th>
-                                <th>suspended</th>
-                                <th>Options</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {drivers.map((driver, index) =>
-                                <tr
-                                    key={driver.id}>
-                                    <td>{index}</td>
-                                    <td>{driver.name}</td>
-                                    <td>{driver.phoneNumber}</td>
-                                    <td>{driver.suspended === true ? <span className="badge rounded-pill bg-danger">true</span> : <span className="badge rounded-pill bg-success">false</span> }</td>
-                                    <td>
-                                        <div className="btn-group">
-                                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
-                                                
-                                            </button>
-                                            <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
-                                                {
-                                                    driver.suspended?
-                                                    <li className="dropdown-item" onClick={() => dispatch(unsuspendDriver(driver.id))}>Unsuspend Driver</li>
-                                                    :
-                                                    <li className="dropdown-item" onClick={() => dispatch(suspendDriver(driver.id))}>Suspend Driver</li>
-                                                    
-                                                    }
-                                                    <li className="dropdown-item" >Delete Driver Account</li>
-                                            </ul>
-                                        </div> 
-                                    </td>
+                    <div className="table-responsive">
+                        <table className="table table-striped table-hover mt-5">
+                            <thead className="table-blue text-light">             
+                                <tr>
+                                    <th>#</th>
+                                    <th>name</th>
+                                    <th>phone number</th>
+                                    <th>suspended</th>
+                                    <th>Options</th>
                                 </tr>
-                                )}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {drivers.map((driver, index) =>
+                                    <tr
+                                        key={driver.id}>
+                                        <td>{index}</td>
+                                        <td>{driver.name}</td>
+                                        <td>{driver.phoneNumber}</td>
+                                        <td>{driver.suspended === true ? <span className="badge bg-danger fs-6">true</span> : <span className="badge bg-success fs-6">false</span> }</td>
+                                        <td>
+                                            <div className="btn-group">
+                                                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
+                                                    
+                                                </button>
+                                                <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
+                                                    {
+                                                        driver.suspended?
+                                                        <li className="dropdown-item" onClick={() => dispatch(unsuspendDriver(driver.id))}>Unsuspend Driver</li>
+                                                        :
+                                                        <li className="dropdown-item" onClick={() => dispatch(suspendDriver(driver.id))}>Suspend Driver</li>
+                                                        
+                                                        }
+                                                        <li className="dropdown-item" >Delete Driver Account</li>
+                                                </ul>
+                                            </div> 
+                                        </td>
+                                    </tr>
+                                    )}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>                                      
         </main>
