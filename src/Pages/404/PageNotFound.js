a39a3975bc380c48476ff984fbc8a0826fce0f92
brianwachira <brianwachira7@gmail.com>
2021-05-30 22:16:08 +0300

    [FIX] : Page not found look

--- src/Pages/404/PageNotFound.js
+++ src/Pages/404/PageNotFound.js
@@ -2,11 +2,25 @@ import React from "react"
 import "./PageNotFound.scss"
 
 import { ReactComponent as PageNotFoundImage } from "../../Assets/Images/404.svg"
+import SideBar from "../../Components/SideBar/SideBar"
 const PageNotFound = () => {
 
     return (
     <>
-        <PageNotFoundImage/>
+        <main className="container-fluid">
+            <div className="row">
+                <div className="col-md-3 ps-0">
+                    <SideBar/>
+                </div>
+                <div className="col-md-3 ps-0">               
+                    <div className="vh-100">
+                        <div className="d-flex justify-content-center mx-auto">
+                            <PageNotFoundImage/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </>
     )
 }
