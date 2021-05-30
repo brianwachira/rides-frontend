f137853aeb99e3f696f7d51aea85217454b73980
brianwachira <brianwachira7@gmail.com>
2021-05-30 18:56:22 +0300

    [FEATURE] : notification

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 
+import './Notification.scss'
 
 const Notification = () => {
 
   const notification = useSelector(state => state.notification)
+
+  if(!notification){
+      return null
+  }
   return (
-      <div className={notification.status}>
-          {notification.message}
+        <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className="toast-header">
+                <img src="..." className="rounded me-2" alt="..."/>
+                <strong className="me-auto">{notification.title}</strong>
+                <small className="text-muted">just now</small>
+                <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div className="toast-body">
+                {notification.message}
+            </div>
       </div>
   )
 }
