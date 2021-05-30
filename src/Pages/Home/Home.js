07e56280b966283666153662175b7b99e21e9ec5
brianwachira <brianwachira7@gmail.com>
2021-05-30 22:34:59 +0300

    [FIX] : add cards showing information

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -39,7 +39,67 @@ const Home = () => {
                     <Link to="/rides">
                         <Card
                             icon={<i className="fa fa-check-circle fa-4x icon-custom" aria-hidden="true"></i>}
-                            label='No. Of Rides Today'
+                            label='No. Of Done Rides'
+                            number={ongoingRides.length}
+                            />
+                    </Link>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/passengers">
+                        <Card
+                            icon={<i className="fa fa-check-circle fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='All Passengers'
+                            number='20'
+                            />
+                    </Link>
+                </div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/passengers">
+                        <Card
+                            icon={<i className="fa  fa-user fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='No. Of Passengers On Rides'
+                            number='20'
+                            />
+                    </Link>
+                </div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/passengers">
+                        <Card
+                            icon={<i className="fa  fa-user fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='No. Of Passengers on Done Rides'
+                            number='20'
+                            />
+                    </Link>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/drivers">
+                        <Card
+                            icon={<i className="fa fa-check-circle fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='No. Of Drivers Today'
+                            number='20'
+                            />
+                    </Link>
+                </div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/drivers">
+                        <Card
+                            icon={<i className="fa  fa-user fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='No. Of Drivers on Rides'
+                            number='20'
+                            />
+                    </Link>
+                </div>
+                <div className="col-md-3 mt-5">
+                    <Link to="/drivers">
+                        <Card
+                            icon={<i className="fa  fa-user fa-4x icon-custom" aria-hidden="true"></i>}
+                            label='No. Of Drivers on Done Rides'
                             number='20'
                             />
                     </Link>
