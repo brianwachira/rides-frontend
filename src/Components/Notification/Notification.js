0f625d39c3f69e1491412e5b094a46a4941d2532
brianwachira <brianwachira7@gmail.com>
2021-05-30 18:33:55 +0300

    [FEATURE] : notification

---
+++ src/Components/Notification/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+
+
+const Notification = () => {
+
+  const notification = useSelector(state => state.notification)
+  return (
+      <div className={notification.status}>
+          {notification.message}
+      </div>
+  )
+}
+
+export default Notification
