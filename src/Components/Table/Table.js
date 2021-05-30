8bbfc3c03b671c207050f746aeafb972e58601f6
brianwachira <brianwachira7@gmail.com>
2021-05-30 18:18:50 +0300

    [FIX] : optimize table by creating separate OptionsButton and Options class

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -3,6 +3,31 @@ import React from "react"
 const Table = (props) => {
     const {rides, handleClick , buttonLabel, setCoordinates} = props
 
+    const OptionsButton = () => {
+        if(buttonLabel){
+            return <th>Options</th>
+        }
+        return null
+    }
+
+    const Options = (rideId) => {
+
+        if(buttonLabel) {
+            return(
+            <td>
+                <div className="btn-group">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
+                        
+                    </button>
+                    <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
+                        <li className="dropdown-item"  onClick={() => handleClick(rideId)} >{buttonLabel}</li>
+                    </ul>
+                </div> 
+            </td> 
+            )
+        }
+        return null
+    }
     return (
         <>
             <div className="table-responsive">
@@ -15,7 +40,7 @@ const Table = (props) => {
                             <th>Driver</th>
                             <th>Passenger</th>
                             <th>Status</th>
-                            {buttonLabel ? <th>Options</th> : ''}
+                            <OptionsButton/>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,26 +48,13 @@ const Table = (props) => {
                             <tr
                                 key={ride.id}
                                 onClick={setCoordinates? () => setCoordinates(ride.pickupPoint,ride.destinationPoint) : null}>
-                                <td>{index}</td>
+                                <td>{index+1}</td>
                                 <td>{ride.pickupPoint.toString()}</td>
                                 <td>{ride.destinationPoint.toString()}</td>
                                 <td>{ride.driver["name"]}</td>
                                 <td>{ride.passenger["name"]}</td>
                                 <td>{ride.status === 'ongoing' ? <span className="badge bg-orange">Ongoing</span> : <span className="badge bg-blue">Done</span>}</td>
-                                {buttonLabel ?
-                                <td>
-                                    <div className="btn-group">
-                                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"  data-bs-target="#dropdownMenuButton" aria-expanded="false">
-                                            
-                                        </button>
-                                        <ul className="dropdown-menu" id="dropdownMenuButton" aria-labelledby="dropdownMenuButton">
-                                            <li className="dropdown-item"  onClick={() => handleClick(ride.id)} >{buttonLabel}</li>
-                                        </ul>
-                                    </div> 
-                                </td>
-                                :
-                                ''
-                                 }
+                                {Options(ride.id)}
                             </tr>
                             )}                    
                     </tbody>
