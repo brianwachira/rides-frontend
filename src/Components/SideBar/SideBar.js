e77973a3827349c57d87c0f5553986a7f6928df5
brianwachira <brianwachira7@gmail.com>
2021-05-31 04:37:03 +0300

    [FIX] : remove unnecessary link

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -66,7 +66,7 @@ const SideBar = () => {
                         <strong>Admin</strong>
                     </Link>
                     <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><Link className="dropdown-item" to='#' onClick={() => logout()}>Sign out</Link></li>
+                        <li><div className="dropdown-item" onClick={() => logout()}>Sign out</div></li>
                     </ul>
                     </div>
                 </div>
